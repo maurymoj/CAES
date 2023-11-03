ed2aab984ee4beaddde0a6465f4cfd6bae04da3d
maurylboro <m.m.oliveira-jr@lboro.ac.uk>
2023-11-03 15:24:39 +0000

    Creation of script for simulation using isothermal assumption

--- Pipeline_tests.m
+++ Pipeline_tests.m
@@ -145,6 +145,7 @@ for j=1:length(Var)
     s = zeros(length(L),1);
     h = zeros(length(L),1);
     psi = zeros(length(L),1);
+    U_erosional = zeros(length(L),1);
     P = zeros(length(L),1);
     T = T_f*ones(length(L),1);      % Assuming isothermal flow
     
@@ -157,12 +158,16 @@ for j=1:length(Var)
         h(i) = py.CoolProp.CoolProp.PropsSI('H','P',P(i),'T',T_f,'Air');
         s(i) = py.CoolProp.CoolProp.PropsSI('S','P',P(i),'T',T_f,'Air');
         Z(i) = py.CoolProp.CoolProp.PropsSI('Z','P',P(i),'T',T_f,'Air');
-        
+        U_erosional(i) = 1.22*100/sqrt(rho(i)); % The constant 100, can be any value from 100 to 250, 
+
         nu_Po(i) = 10*nu(i);
     
         % Velocity m/s
         u(i) = 14.7349*( Q_a_day/D_mm^2 )*( P_a/T_a )*(Z(i)*T_f/P(i)); % Q_b has to be converted to m3/day and D to mm
-        
+        if u(i) > 0.5*U_erosional(i)
+            warning('U greater than 50% of the erosional velocity')
+        end
+
         % Exergy
         % phi = h-h0 + T0*(s-s0)+u^2/2+gz
         % Negligible height difference
@@ -421,7 +426,7 @@ gam = cp/cv;
 
 m_dot = rho_a*Q_a;
 % j=1;
-load('StF_Ab.mat');
+load('StF_Ab2.mat');
 
 for j=1:length(Var)
     % H_2 = Var{j};
@@ -438,6 +443,8 @@ for j=1:length(Var)
     epsD = eps/D;
     f_guess = (2*log10(1/epsD)+1.14)^(-2); % Initial estimation using Nikuradse eq.
     
+    m_dot = rho_a*Q_a;
+
     P_a_kPa = P_a/1000;
     Q_a_day = Q_a*24*3600;    % Sm3/day
     L_km = L./1000;           % km
@@ -452,8 +459,10 @@ for j=1:length(Var)
     s = zeros(length(L),1);
     h = zeros(length(L),1);
     psi = zeros(length(L),1);
+    U_erosional = zeros(length(L),1);
     P = zeros(length(L),1);
     T = T_a*ones(length(L),1);
+    
     
     P(1) = P_in;
     T(1) = T_in;
@@ -475,12 +484,16 @@ for j=1:length(Var)
             h(i) = py.CoolProp.CoolProp.PropsSI('H','P',P(i),'T',T_f,'Air');
             s(i) = py.CoolProp.CoolProp.PropsSI('S','P',P(i),'T',T_f,'Air');
             Z(i) = py.CoolProp.CoolProp.PropsSI('Z','P',P(i),'T',T_f,'Air');
-            
+            U_erosional(i) = 1.22*100/sqrt(rho(i)); % The constant 100, can be any value from 100 to 250, 
+
             nu_Po(i) = 10*nu(i);
         
             % Velocity m/s
             u(i) = 14.7349*( Q_a_day/D_mm^2 )*( P_a/T_a )*(Z(i)*T_f/P(i)); % Q_b has to be converted to m3/day and D to mm
-            
+            if u(i) > 0.5*U_erosional(i)
+                warning('U greater than 50% of the erosional velocity')
+            end
+
             % Exergy
             % phi = h-h0 + T0*(s-s0)+u^2/2+gz
             % Negligible height difference
@@ -519,6 +532,16 @@ for j=1:length(Var)
 
                 dh = dh_SfA(i); % Custom elevation profile
 
+                % P from General Flow Equation
+                % Negligible height difference
+                % P(i+1) = sqrt( P_1_kPa^2 - ( Q_a_day/(1.1494e-3*(T_a/P_a_kPa)*D_mm^2.5) )^2*G*T_f*(dL/1000)*Z_f*f(i) );
+                % % Considering height difference
+                % s_H = 0.0684*G*(dh)/(T_f*Z_f);                                % Constant gradient / Custom gradient 
+                % s_H = 0.0684*G*(dh+dh_sin*sin(2*pi/T_sin*(i*dL) ))/(T_f*Z_f); % Sinusoidal profile
+                % s_H = 0.0684*G*(dh))/(T_f*Z_f); % Profile St. F. -> Ab 
+                % L_e = dL*(exp(s_H)-1)/s_H;
+                % P(i+1) = sqrt( (P_1_kPa^2 - ( Q_a_day/(1.1494e-3*(T_a/P_a_kPa)*D_mm^2.5) )^2*G*T_f*(L_e/1000)*Z_f*f(i) )/exp(s_H) );
+
                 % P from Panhandle B equation
                 % Negligible height difference
                 % P(i+1) = sqrt( P_1_kPa^2 - ( Q_a_day/(1.002e-2*E*(T_a/P_a_kPa)^1.02*D_mm^2.53) )^(1/0.51)*G^0.961*T_f*(dL/1000)*Z_f );
@@ -526,7 +549,11 @@ for j=1:length(Var)
                 s_H = 0.0684*G*(dh)/(T_f*Z_f);
                 L_e = dL*(exp(s_H)-1)/s_H;
                 P(i+1) = sqrt( (P_1_kPa^2 - ( Q_a_day/(1.002e-2*E*(T_a/P_a_kPa)^1.02*D_mm^2.53) )^(1/0.51)*G^0.961*T_f*(L_e/1000)*Z_f )/exp(s_H) );
-                
+                % Panhandle B equation - valid for large diameter, high pressure flows with 4M < Re < 40M
+                if (Re(i) < 4e6) || (Re(i) > 40e6)
+                    warning('Re outside the indicated region for the Panhandle B equation.')
+                end
+
                 dP = (P(i+1) - P_2_kPa)/P_2_kPa;
                 P_2_kPa = P(i+1);
                 count = count + 1;
