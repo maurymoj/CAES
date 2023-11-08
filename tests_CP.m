9f62f993a2104daa44038bb3857ce5bdfa579009
maurylboro <m.m.oliveira-jr@lboro.ac.uk>
2023-11-08 12:56:05 +0000

    implementation of Pottie simplification in the actual code

--- tests_CP.m
+++ tests_CP.m
@@ -1,5 +1,5 @@
 % pressure over 70 km pipeline
 % pyversion c:\Anaconda3\python.exe % Sets Python 3.11 as a interpreter
 CP = py.importlib.import_module('CoolProp.CoolProp');
-
+%%
 CP.PropsSI('D','P',101325,'T',298,'Air')
